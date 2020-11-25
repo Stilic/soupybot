7787719e6373fafc7bb16dc321dedb9ad8732f5d
Stylix58 <63605602+Stylix58@users.noreply.github.com>
2020-11-25 14:56:42 +0000

    del categorie general

--- bot/main.py
+++ bot/main.py
@@ -27,21 +27,20 @@ async def on_ready() :
   await client.change_presence(status = discord.Status.idle, activity = discord.Game("For help: type .help"))
   print("INFO: Bot is online\nINFO: For quit, press Ctrl+C")
 
-class General(commands.Cog):
-  @client.command()
-  async def ping(ctx):
-    """I am fast or no?"""
-    await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
+@client.command()
+async def ping(ctx):
+  """I am fast or no?"""
+  await ctx.send(f"🏓 Pong with {str(round(client.latency, 2))}")
 
-  @client.command(name="whoami")
-  async def whoami(ctx):
-    """Shows what guy have send this command."""
-    await ctx.send(f"You are {ctx.message.author.name}")
+@client.command(name="whoami")
+async def whoami(ctx):
+  """Shows what guy have send this command."""
+  await ctx.send(f"You are {ctx.message.author.name}")
 
-  @client.command()
-  async def clear(ctx, amount=3):
-    """Clear a number of messages you specify (by default 3)"""
-    await ctx.channel.purge(limit=amount)
+@client.command()
+async def clear(ctx, amount=3):
+  """Clear a number of messages you specify (by default 3)"""
+  await ctx.channel.purge(limit=amount)
 
 client.add_cog(General())
 
